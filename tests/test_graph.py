a813727db88914e25f16b16404f6857a8384e4ea
Randy Herritt <randy.herritt@gmail.com>
2025-08-06 16:00:13 -0300

    Tested adding node to graph and made sure it was of type Node

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -12,3 +12,15 @@ def test_graph_has_nodes_list():
     assert isinstance(graph.nodes, list)
     assert len(graph.nodes) == 0
 
+def test_node_creation():
+    node = Node()
+    assert node is not None
+    assert isinstance(node, Node)
+
+def test_add_node_to_graph():
+    graph = Graph()
+    node = Node()
+    graph.nodes.append(node)
+    assert len(graph.nodes) == 1
+    assert graph.nodes[0] is node
+    assert isinstance(graph.nodes[0], Node)
